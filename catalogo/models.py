525b57a11088f9e9e15a69b0deca2617034c1c70
enzo <enzostefani507@gmail.com>
2021-06-18 13:45:57 -0300

    se creo modelo y registro en admin

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(
+        max_length = 500,
+        null = False,
+        blank = False,
+        editable=True,
+    )
+    imagen = models.CharField(
+        max_length = 500,
+        null = False,
+        blank = False,
+        editable = True
+    )
+    en_stock_actual = models.BooleanField(
+        default = False,
+        null = False,
+        blank = False,
+        editable = True
+    )
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Producto
 
+class ProductosList(ListView):
+    model = Producto
+    template_name = 'catalogo/catalogo_list.html'
