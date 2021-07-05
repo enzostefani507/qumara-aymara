d7471d707d5c94324d4ade9ade51e083a8555da7
enzo <enzostefani507@gmail.com>
2021-07-05 00:45:06 -0300

    view list generica creada

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'post_list'
+    paginate_by =  8
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['title'] = "Qumara Aymara - Blog"
+        return context
