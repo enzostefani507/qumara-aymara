a90e61c2934d153e37dd5b38545200ec32bd8d5d
Benites Stefani Enzo Ricardo <enzostefani507@gmail.com>
2021-07-16 20:15:21 -0300

    var entorno

--- pagina/settings.py
+++ pagina/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eptfx^@gly1@ntnclm-)_ff&ht=)ing%$u*)ya3sqb7$oagfk3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['qumara-aymara.herokuapp.com','qumaraaymara.com.ar','*']
