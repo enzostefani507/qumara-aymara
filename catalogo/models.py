4cef70aaee51a588c07bba53fd319a08f9d30a76
Benites Stefani Enzo Ricardo <enzostefani507@gmail.com>
2021-07-17 10:13:35 -0300

    tamaño de nombre arreglado

--- catalogo/models.py
+++ catalogo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(
-        max_length = 500,
+        max_length = 20,
         null = False,
         blank = False,
         editable=True,
