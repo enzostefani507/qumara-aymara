53abd6d0e5543dfc1415a7e192b6e23ebffef758
Benites Stefani Enzo Ricardo <enzostefani507@gmail.com>
2021-07-20 12:32:53 -0300

    mi perfil, correcciones nav

--- perfil/urls.py
+++ perfil/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('/login', views.LoginUsuario.as_view(), name='login'),
     path('/logout', views.LogoutUsuario.as_view(), name="logout"),
+    path('/mi_perfil<int:pk>', views.miPerfil.as_view(),name='perfil')
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.views import LoginView,LogoutView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from . import forms
 
@@ -9,3 +11,10 @@ class LoginUsuario(LoginView):
     form_class = forms.LoginForm
     template_name = 'perfil/login.html'
     success_url = reverse_lazy('')
+
+class miPerfil(DetailView):    
+    template_name = 'perfil/perfil.html'
+    model = User
+
+    def get_queryset(self, *args, **kwargs):
+        return User.objects.filter(pk=self.request.user.id)
