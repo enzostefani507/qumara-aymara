8758a611186f4964858118d8b3a9de4ac3c7e61d
Benites Stefani Enzo Ricardo <enzostefani507@gmail.com>
2021-08-02 17:50:12 -0300

    funcion añadir/eliminar post de favoritos del usuario

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, tratar_favorito
 
 urlpatterns = [
     path('', PostList.as_view(), name='posts'),
-    path('/<int:pk>', PostDetail.as_view(),name='detalle_post')
+    path('/<int:pk>', PostDetail.as_view(),name='detalle_post'),
+    path('/fav/<int:id>', tratar_favorito,name='tratar_favorito'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
+from django.views.generic.base import RedirectView
 from .models import Post
+from perfil.models import Usuario
+from django.contrib.auth.decorators import login_required
 
 class PostList(ListView):
     model = Post
@@ -16,3 +20,26 @@ class PostDetail(DetailView):
     template_name = 'post/detalle.html'
     model = Post
     context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user = Usuario.objects.get(id=self.request.user.id)
+            post = Post.objects.get(id=self.object.id)
+            if post in user.post_favoritos.all():
+                context['en_favoritos'] = True
+            else:
+                context['en_favoritos'] = False
+        context['title'] = "Qumara Aymara - Blog"
+        return context
+
+@login_required(login_url='/perfil/login')
+def tratar_favorito(request,id):
+    post = get_object_or_404(Post, id = id)
+    user = Usuario.objects.get(id=request.user.id)
+    if post in user.post_favoritos.all():
+        user.post_favoritos.remove(post)
+    else:
+        user.post_favoritos.add(post)
+    return redirect('detalle_post',id)
+        
